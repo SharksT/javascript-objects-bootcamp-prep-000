29d26d8dbb10cd2c2d859645d41b04a8c9353345
Felipe Passos <felipekaires@hotmail.com>
2017-10-21 13:44:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@
 var playlist = { name : '', song : ''}
 
 function updatePlaylist(obj,name,song) {
-  return Object.assing({},obj,{[name]:song})
+  return Object.assign({},obj,{[name]:song})
 }
 
 function removeFromPlaylist(obj,name) {
